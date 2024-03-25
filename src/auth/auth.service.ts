918c8b46d811a7209e5dd45a34f4c1da4bb8a65a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-24 22:39:52 -0700

    usuario guardado en base de datos

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,50 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('AuthService');
 
-
-
   onModuleInit() {
     this.$connect();
     this.logger.log('AuthDB Connected');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { name, email, password } = registerUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user)
+        throw new RpcException({
+          status: 400,
+          message: 'User already registered',
+        });
+
+      const newUser = await this.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+
+      return {
+        user: newUser,
+        toke: 'ABC',
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 }
